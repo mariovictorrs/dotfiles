40dcd13a4dfc90653eead8c7fcc4371831fcc7ea
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-09-03 09:38:45 -0300

    feat(hyprland): `ALT + TAB` to change to previous workspace

--- .config/hypr/configs/keybindings.lua
+++ .config/hypr/configs/keybindings.lua
@@ -64,6 +64,9 @@ hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:mag
 hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
 hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
 
+-- Alt+Tab to switch to previous workspace
+hl.bind("ALT + TAB", hl.dsp.focus({ workspace = "previous_per_monitor" }))
+
 -- Move/resize windows with mainMod + LMB/RMB and dragging
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
