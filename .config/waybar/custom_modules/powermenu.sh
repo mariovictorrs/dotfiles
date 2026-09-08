8815ccee2aeca085f3783f81742568f4979d3d1e
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-09-08 20:06:31 -0300

    chore(waybar): remove waybar

--- .config/waybar/custom_modules/powermenu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-chosen=$(printf "󰤄 Suspend\n󰜉 Reboot\n󰐥 Shutdown" | rofi -dmenu -p "")
-case "$chosen" in
-  *Suspend)  systemctl suspend ;;
-  *Reboot)   systemctl reboot ;;
-  *Shutdown) systemctl poweroff ;;
-esac
