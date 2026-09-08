4435d4cc9fa787a26f4af094bdd705d66e99be31
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-09-08 16:30:15 -0300

    feat(nvim): add gh cli into snacks

--- .config/nvim/lua/mario/plugins/snacks.lua
+++ .config/nvim/lua/mario/plugins/snacks.lua
@@ -128,5 +128,33 @@ return {
       end,
       desc = "File explorer",
     },
+    {
+      "<leader>gp",
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = "GitHub Pull Requests (open)",
+    },
+    {
+      "<leader>gP",
+      function()
+        Snacks.picker.gh_pr({ state = "all" })
+      end,
+      desc = "GitHub Pull Requests (all)",
+    },
+    {
+      "<leader>gi",
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = "GitHub Issues (open)",
+    },
+    {
+      "<leader>gI",
+      function()
+        Snacks.picker.gh_issue({ state = "all" })
+      end,
+      desc = "GitHub Issues (all)",
+    },
   },
 }
